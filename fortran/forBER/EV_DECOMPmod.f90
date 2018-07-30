ba18bc6ef681559e520788290284f20124e3d3d4
gryu643 <ps34h.k.r@gmail.com>
2018-07-30 17:01:26 +0900

    プログラムの概形作成

---
+++ fortran/forBER/EV_DECOMPmod.f90
@@ -0,0 +1,27 @@
+module EV_DECOMPmod
+    implicit none
+contains
+    function EV_DECOMP(H,Nsybl,Npath)
+        implicit none
+
+        !argument
+        integer Nsybl,Npath
+        complex(kind(0d0)) H(Nsybl,Nsybl)
+
+        !return
+        complex(kind(0d0)) EV_DECOMP(Nsybl,Nsybl)
+
+        !declaration
+        !
+        ! to do
+        !
+
+        !initialization
+        !
+        ! to do
+        !
+
+        !implimentation part
+
+    end function
+end module EV_DECOMPmod
