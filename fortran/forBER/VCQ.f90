23ba28b9b658d384999fdced195ce6dfd3f7449c
ryusuke <ps34h.k.r@gmail.com>
2018-10-29 04:25:02 +0900

    BERシミュレーション部削除

--- fortran/forBER/VCQ.f90
+++ fortran/forBER/VCQ.f90
@@ -12,15 +12,11 @@ program VCQ
     integer,parameter :: SEbN0=-10
     integer,parameter :: EEbN0=40
     integer,parameter :: Step=5
-    integer,parameter :: Nloop=10000
+    integer,parameter :: Nloop=10
     double precision,parameter :: BERStandard=1.0d-2
 
     integer i,j
     double precision Ampd(Npath,1)
-    double precision Psig
-    double precision Pwgn
-    integer Collect
-    integer False
     integer loop
     integer KEbN0
     complex(kind(0d0)) Cpath(Npath,1)
@@ -29,23 +25,7 @@ program VCQ
     complex(kind(0d0)) Xppl(Nsybl,Nsybl)
     complex(kind(0d0)) HH(Nsybl,Nsybl+Npath-1)
     complex(kind(0d0)) HHH(Nsybl,Nsybl)
-    complex(kind(0d0)) S(1,Nsybl)
-    integer TdatI(1,Nsybl)
-    integer TdatQ(1,Nsybl)
     complex(kind(0d0)) V(Nsybl,Nsybl)
-    complex(kind(0d0)) SU(Nsybl,Nsybl)
-    complex(kind(0d0)) X(Nsybl,Nsybl)
-    double precision Pow
-    complex(kind(0d0)) Y(Nsybl+Npath-1,1)
-    complex(kind(0d0)) Noise(Nsybl+Npath-1,1)
-    complex(kind(0d0)) Yn(Nsybl,1)
-    complex(kind(0d0)) Y2(Nsybl,1)
-    integer RdatI(1,Nsybl)
-    integer RdatQ(1,Nsybl)
-    complex(kind(0d0)) A(Nsybl,1)
-    complex(kind(0d0)) Y2H(1,Nsybl)
-    complex(kind(0d0)) R(1,1)
-    complex(kind(0d0)) R2
     double precision EbN0
     double precision BER
     double precision Eig(1,Nsybl)
@@ -59,10 +39,6 @@ program VCQ
 
     !initialize
     Ampd=0.0d0
-    Psig=0.0d0
-    Pwgn=0.0d0
-    Collect=0
-    False=0
     loop=0
     KEbN0=0
     Cpath=(0.0d0,0.0d0)
@@ -71,23 +47,7 @@ program VCQ
     Xppl=(0.0d0,0.0d0)
     HH=(0.0d0,0.0d0)
     HHH=(0.0d0,0.0d0)
-    S=0.0d0
-    TdatI=0
-    TdatQ=0
     V=(0.0d0,0.0d0)
-    SU=(0.0d0,0.0d0)
-    X=(0.0d0,0.0d0)
-    Pow=0.0d0
-    Y=(0.0d0,0.0d0)
-    Noise=(0.0d0,0.0d0)
-    Yn=(0.0d0,0.0d0)
-    Y2=(0.0d0,0.0d0)
-    RdatI=0
-    RdatQ=0
-    A=(0.0d0,0.0d0)
-    Y2H=(0.0d0,0.0d0)
-    R=(0.0d0,0.0d0)
-    R2=(0.0d0,0.0d0)
     EbN0=0.0d0
     BER=0.0d0
     Eig=0.0d0
@@ -98,8 +58,6 @@ program VCQ
     call system_clock(t1)
 
     !file open
-    open (1, file='VCQ(s32p4).csv', status='replace')
-    open (2, file='VCtrial.csv', status='replace')
     open (3, file='VCQ_UseChNum.csv', status='replace')
 
     !implimentation part
@@ -110,10 +68,6 @@ program VCQ
     end do
 
     do KEbN0=SEbN0, EEbN0, Step !Eb/N0 loop
-        Psig = 0.0d0
-        Pwgn = 0.0d0
-        Collect = 0
-        False = 0
         AvUseChNum=0.0d0
         AvRTNum=0.0d0
         !setup Convergence standard
@@ -179,110 +133,14 @@ program VCQ
                     AvUseChNum = AvUseChNum + dble(Nsybl)/dble(Nloop)
                 endif
             end do
-
-            !set information symbol
-            do i=1, Nsybl
-                S(1,i) = cmplx(nint(rand())*2.0d0-1.0d0,nint(rand())*2.0d0-1.0d0,kind(0d0)) !-1or1
-            end do
-            do i=1, Nsybl
-                TdatI(1,i) = (real(S(1,i))+1)/2 !0or1
-                TdatQ(1,i) = (aimag(S(1,i))+1)/2
-            end do
-            do i=1, Nsybl
-                do j=1, Nsybl
-                    SU(j,i) = S(1,i) * V(j,i)
-                end do
-            end do
-
-            !transmit vector
-            X = (0.0d0,0.0d0)
-            do i=1,Nsybl
-                do j=1,Nsybl
-                    X(j,1) = X(j,1) + SU(j,i)
-                end do
-            end do
-
-            !power
-            Pow = 0.0d0
-            do i=1, Nsybl
-                Pow = Pow + (abs(X(i,1))**2.0d0)/Nsybl
-            end do
-            X = X / sqrt(Pow)
-            call CMultiply(H,X,Y,Nsybl+Npath-1,Nsybl,Nsybl,1) !pass H
-
-            do i=1, Nsybl+Npath-1
-                Noise(i,1) = cmplx(normal(),normal(),kind(0d0))
-            end do
-            !正規乱数の分散＝１＝雑音電力なので、正規乱数に雑音電力をかける（√２で割っているのはIとQの両方合わせて雑音電力とするため）
-            Noise = Noise / sqrt(2.0d0) * sqrt(1.0d0/(10.0d0**(KEbN0/10.0d0))/2.0d0)
-
-            do i=1, Nsybl+Npath-1
-                Psig = Psig + (abs(Y(i,1))**2.0d0)
-                Pwgn = Pwgn + abs(Noise(i,1))**2.0d0
-            end do
-
-            !add noise
-            call CAdd(Y,Noise,Y,Nsybl+Npath-1,1,Nsybl+Npath-1,1)
-
-            !Matched Filter
-            call CMultiply(HH,Y,Yn,Nsybl,Nsybl+Npath-1,Nsybl+Npath-1,1)
-        
-            do i=1, Nsybl
-                Y2(i,1) = Yn(i,1)
-            end do
-            RdatI = 0
-            RdatQ = 0
-            !Demodulation
-            do i=1, Nsybl
-                do j=1, Nsybl
-                    A(j,1) = V(j,i)
-                end do
-                !inner product
-                call CAdjoint(Y2,Y2H,Nsybl,1)
-                call CMultiply(Y2H,A,R,1,Nsybl,Nsybl,1)
-                R(1,1) = conjg(R(1,1))
-                R2 = R(1,1)
-
-                if(real(R2)>0.0) then
-                    RdatI(1,i) = 1
-                elseif(real(R2)<0.0) then
-                    RdatI(1,i) = 0
-                endif
-                if(aimag(R2)>0.0) then
-                    RdatQ(1,i) = 1
-                elseif(aimag(R2)<0.0) then
-                    RdatQ(1,i) = 0
-                endif
-            end do
-
-            !Bit Error Rate
-            do i=1, Nsybl
-                if(RdatI(1,i)==TdatI(1,i)) then
-                    Collect = Collect + 1
-                elseif(RdatI(1,i)/=TdatI(1,i)) then
-                    False = False + 1
-                endif
-                if(RdatQ(1,i)==TdatQ(1,i)) then
-                    Collect = Collect + 1
-                elseif(RdatQ(1,i)/=TdatQ(1,i)) then
-                    False = False + 1
-                endif
-            end do
-            write(2,*) loop, ',', dble(False)/(dble(Collect)+dble(False))
         end do
-        
-        EbN0 = 10.0d0*dlog10(Psig/Pwgn/2.0d0) !QPSK rate =2
-        BER = dble(False) / (dble(Collect) + dble(False))
-        write(1,*) EbN0, ',', BER, ',', RTNum
-        print *, 'EbN0=', EbN0
-        print *, 'BER=', BER
+
+        print *, 'KEbN0=', KEbN0
         print *, 'AvRTNum=', AvRTNum
         write(3,*) KEbN0, ',', AvUseChNum
     end do
 
     !file close
-    close(1)
-    close(2)
     close(3)
 
     !time measurement end
