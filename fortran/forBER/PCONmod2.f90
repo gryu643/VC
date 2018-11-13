7faecbcdca15d9abd33676d9702ea67cc5a0df64
gryu643 <ps34h.k.r@gmail.com>
2018-11-13 21:55:20 +0900

    手計算した結果を使用して電力分配

--- fortran/forBER/PCONmod2.f90
+++ fortran/forBER/PCONmod2.f90
@@ -12,37 +12,48 @@ contains
         double precision Pcon(1,Nsybl)
 
         double precision mu
-        double precision parts1,parts2
-        integer flag
+        double precision p1,p2
+        double precision Min,Sum
         
-
         !--initialization
         mu=0.0d0
-        parts1=0.0d0
-        parts2=0.0d0
-        flag=0
+        p1=0.0d0
+        p2=0.0d0
+        info=1
+        Min=0.0d0
+        Sum=0.0d0
 
         !--implementation
-        do i=1, Nsybl
-            parts1 = parts2 + 1.0d0/lambda(1,i)/EbN0*dlog(lambda(1,i)/dble(Nsybl)*EbN0)
-        end do
-        do i=1, Nsybl
-            parts2 = parts2 + 1.0d0/lambda(1,i)/EbN0
+        do i=1, Rep
+            p1 = p1 + 1.0d0/lambda(1,i)
+            p2 = p2 + 1.0d0/lambda(1,i)*dlog(dble(Rep)/lambda(1,i)/EbN0)
         end do
 
-        mu = -1.0d0*dexp((parts1-1.0d0)/parts2)
-        do i=1, Nsybl
-            if(lambda(1,i)/dble(Nsybl)*EbN0>=-mu) then
-                Pcon(1,i) = 1.0d0/lambda(1,i)/EbN0*(dlog(lambda(1,i)/dble(Nsybl)*EbN0)-dlog(-mu))
-            else
-                Pcon(1,i) = 0.0d0
-                flag=flag+1
+        mu = -EbN0/p1*(dble(Rep)+p2/EbN0)
+
+        do i=1, Rep
+            Pcon(1,i) = -1.0d0/lambda(1,i)/EbN0*(mu+dlog(dble(Rep)/lambda(1,i)/EbN0))
+        end do
+
+        do i=1, Rep
+            if(0.0d0>Pcon(1,i).and.Pcon(1,i)<Min) then
+                Min=Pcon(1,i)
+                info=-1
             endif
         end do
-        if(flag==Nsybl) then
-            info=-1
-        else
-            info=Nsybl-flag
+
+        !set 0.01 for minimum value when minimum value is negative.
+        if(info==-1) then
+            do i=1, Rep
+                Pcon(1,i) = Pcon(1,i) - (Min-0.01d0)
+            end do
+            !normalize Pcon
+            do i=1, Rep
+                Sum = Sum + Pcon(1,i)
+            end do
+            do i=1, Rep
+                Pcon(1,i) = Pcon(1,i) / Sum * dble(Rep)
+            end do
         endif
     end subroutine Pcontrol2
 end module PCONmod2
